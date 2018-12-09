4584c9824ec89fcf74908e66863c572d7ad8c10f
Victor <Victor_Ramos_001@hotmail.com>
2018-12-09 14:48:23 -0700

    Update index.js

---
+++ routes/routes.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/data');
+
+var mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'connection error:'));
+mdb.once('open', function (callback) {
+
+});
+
+var personSchema = mongoose.Schema({
+    username: String,
+    age: String,
+    password: String,
+    email: String
+  });
+
+  var Person = mongoose.model('People_Collection', personSchema);
+
