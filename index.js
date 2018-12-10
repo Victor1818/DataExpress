eda009d4b86c37e9b86daf0e8c32888cfb54710a
Dakota Lee Miller <mdmdakota@gmail.com>
2018-12-09 20:03:47 -0700

    Account
    I believe my part is done, but Mongo is being dumb with me at the moment, or me or both IDK, so I can't test it

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-var express = require('express'),
-  pug = require('pug'),
-  path = require('path'),
-  route = require('./routes/routes.js'),
-  bcrypt = require('bcrypt-nodejs'),
-  cookieParser = require('cookie-parser'),
-  expressSession = require('express-session'),
-  bodyParser = require('body-parser');
+var express = require('express');
+var pug = require('pug');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var route = require('./routes/routes.js');
+var bodyParser = require('body-parser');
 
 
 var app = express();
@@ -22,6 +21,13 @@ var urlencodedParser = bodyParser.urlencoded({
   extended: true
 })
 
-
+app.get('/', route.index);
+app.get('/register', route.register);
+app.post('/register', urlencodedParser, route.registerPerson);
+app.get('/edit/:id', route.edit);
+app.post('/edit/:id', urlencodedParser, route.editPerson);
+app.get('/login/', route.login);
+app.post('/login/', urlencodedParser, route.loginPerson);
+app.get('/logout/:id', route.logout);
 
 app.listen(3000);
